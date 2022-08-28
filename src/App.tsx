11745647338b599b5d68bd4dd6f239af5f588331
Marcin Borowski <borowskimarvin@gmail.com>
2022-08-28 18:15:04 +0200

    add initial data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import apiData from "./api";
-import PersonInfo from "./PersonInfo";
+import PersonInfo from "./components/PersonInfo/PersonInfo";
+
+interface IPerson {
+  id: string;
+  jobTitle: string;
+  emailAddress: string;
+  firstNameLastName: string;
+}
 
 function App() {
-  const [data, setData] = React.useState([]);
-  const [selected, setSelected] = React.useState([]);
+  const [data, setData] = React.useState<IPerson[]>([]);
+  const [selected, setSelected] = React.useState<IPerson[]>([]);
 
-  //  TODO fetch contacts using apiData function, handle loading and error states
+  React.useEffect(() => {
+    apiData()
+      .then((res) => setData((d) => [...d, ...res]))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="App">
       <div className="selected">Selected contacts: {selected.length}</div>
       <div className="list">
         {data.map((personInfo) => (
-          // @ts-ignore
           <PersonInfo key={personInfo.id} data={personInfo} />
         ))}
       </div>
